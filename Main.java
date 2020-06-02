c357d4e40098e61e2946b62b88620530525c765d
Andrey Side <sideandrey@gmail.com>
2020-06-02 11:41:22 +0300

    0206

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         float original = 900;
         float add = 1100;
-        float total = (original + add + (add/100)) ;
-        float bonus = add/100
+        float total = (original + add + bonus) ;
+        float bonus = add/100;
 
          if (add < 1000)
             { bonus = add/100;}
